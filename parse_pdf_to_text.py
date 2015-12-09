1940d487dd8f93ef32ae3bc441f21830c2d83b60
Thomas Boerstad <thomas.borstad@gmail.com>
2015-12-09 16:38:46 +0100

    Hack 'n slash pdf parsing

---
+++ parse_pdf_to_text.py
@@ -0,0 +1,25 @@
+import csv
+
+text=[]
+with open('get.txt','r',encoding='utf-8') as f:
+        text=f.read()
+        a=text.split
+
+a = text.split('\n')
+b = [x for x in a if x]
+print(b)
+i = b.index('TOTALT $ BETALE')
+price = b[i-1].replace(',','.')
+i = b.index('Betalingsfrist')
+date = b[i+1]
+i = b.index('PERIODE')
+period = "bredbaand ({0})".format(b[i+2])
+
+bill = ['',date,period,price,'','','']
+
+
+print(price,date,period)
+
+with open('invoices.csv','a+') as csvfile:
+        writer = csv.writer(csvfile)
+            writer.writerow(bill)        
